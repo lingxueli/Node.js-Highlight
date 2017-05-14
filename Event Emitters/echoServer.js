a6e861b8436794ac971e68fc9008a45ab19ff06e
lingxueli <ll2871@columbia.edu>
2017-05-14 17:01:55 -0600

    update comment

--- Event Emitters/echoServer.js
+++ Event Emitters/echoServer.js
@@ -2,13 +2,14 @@ var net = require('net');
 
 var server = net.createServer(function(socket) {
 	// function(socket): a listener of the 'connection' event. The 'connection' event is emitted when a new connection is made.
-	// socket: the 'connection' Event or <net.Socket>. It implements a duplex Stream interface (EventEmitter). 
+	// socket: Event: 'connection' and instance of class <net.Socket>. It implements a duplex Stream interface (EventEmitter). 
 
 	socket.on('data', function(data){
 		socket.write(data);
 	});
 	// Eventemitter.on(eventName, listener)
 	// function(data): a listener of the 'data' event
+	// 'data' Event: of class <net.Socket>; emitted when data is received.
 });
 
 server.listen(8888);
