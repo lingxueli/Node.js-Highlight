80adcb21be06835f04567f3f68d0cbbf6f9dd10c
lingxueli <ll2871@columbia.edu>
2017-05-16 21:25:54 -0600

    fix bugs

--- Event Emitters/fileMirror.js
+++ Event Emitters/fileMirror.js
@@ -29,7 +29,7 @@ Watcher.prototype.start = function() {
   });
 }
 
-
+watcher = new Watcher(watchDir, processedDir);
 watcher.on('process', function process(file) {
   var watchFile      = this.watchDir + '/' + file;
   var processedFile  = this.processedDir + '/' + file.toLowerCase();
