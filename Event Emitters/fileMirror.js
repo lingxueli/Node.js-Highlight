ad9b17a188b144b3527f1965d63d1735ab744c73
lingxueli <ll2871@columbia.edu>
2017-05-16 21:39:50 -0600

    comment

--- Event Emitters/fileMirror.js
+++ Event Emitters/fileMirror.js
@@ -4,7 +4,7 @@ function Watcher(watchDir, processedDir) {
   this.processedDir = processedDir;
 }
 
-// the Watcher class inherit EventEmitter's behavior
+// the Watcher class inherits EventEmitter's behavior
 var events = require('events'), util = require('util');
 util.inherits(Watcher, events.EventEmitter);
 
@@ -12,19 +12,24 @@ var fs = require('fs')
   , watchDir = './watch'
   , processedDir  = './done';
 
-// extend the class with two methods
+// extends the Watcher class with two methods
 Watcher.prototype.watch = function() { 
   var watcher = this;
+  // fs.readdir(watchDir, callback): read the contents of watchDir; files: the array of files in the directory
   fs.readdir(this.watchDir, function(err, files) {
     if (err) throw err;
     for(index in files) {
+      // fire 'process' event for each files in watchDir
       watcher.emit('process', files[index]); 
     }
   })
 }
+
 Watcher.prototype.start = function() { 
   var watcher = this;
+  // fs.watchFile(watchDir, callback): watch for changes on watchDir; fire callback each time the directory is accessed 
   fs.watchFile(watchDir, function() {
+    // each time the directory is accessed, call watch()
     watcher.watch();
   });
 }
@@ -34,6 +39,7 @@ watcher.on('process', function process(file) {
   var watchFile      = this.watchDir + '/' + file;
   var processedFile  = this.processedDir + '/' + file.toLowerCase();
 
+  // reanme the file to lowercase letters and move it
   fs.rename(watchFile, processedFile, function(err) {
     if (err) throw err;
   });
